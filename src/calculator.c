124cca9de2448ec9e1d6fd2a38a3e503c47130af
Stephanie Kimbery <stephank@pr-e3.kzn.21-school.ru>
2023-02-09 22:45:05 +0300

    SmartCalc v.0

---
+++ src/calculator.c
@@ -0,0 +1,10 @@
+#include "s21_SmartCalc.h"
+
+double polish_notation(char **string) {
+    double result = 0.0;
+    return result;
+}
+
+int main() {
+    return 0;
+}
---
+++ src/s21_SmartCalc.h
@@ -0,0 +1,10 @@
+#ifndef SRC_S21_SMARTCALC_H_
+#define SRC_S21_SMARTCALC_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double polish_notation(char **string);
+
+#endif // SRC_S21_SMARTCALC_H_
