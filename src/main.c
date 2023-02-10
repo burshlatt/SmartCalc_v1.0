22ec046588bf3b67ac02adcc6de07b9b01b563e4
Stephanie Kimbery <stephank@pr-e3.kzn.21-school.ru>
2023-02-10 21:02:15 +0300

    SmartCalc v.1.4

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 
 int main() {
     double result = 0.0;
-    // result = polish_notation("((2+(33-4))+cos(sin(tan(atan(acos(asin(sqrt(34^(92+32)))))))))-3-7+2mod3-10^2-cos(3)+atan(55)");
-    result = polish_notation("(1+(2+3+4))+4+5-3");
+    result = polish_notation("((2+(33-4))+cos(sin(tan(atan(acos(asin(sqrt(34^(92+32)))))))))-3-7+2mod3-10^2-cos(3)+atan(55)");
+    // result = polish_notation("(1+(2+3+4))+4+5-3");
     printf("Result = %lf\n", result);
     return 0;
 }
