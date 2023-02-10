be55634dad601ea075db12c719b4af52590b4ea6
Stephanie Kimbery <stephank@pr-f3.kzn.21-school.ru>
2023-02-10 12:49:50 +0300

    SmartCalc v.1.1

--- src/calculator.c
+++ src/calculator.c
@@ -1,167 +1,263 @@
 #include "s21_SmartCalc.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-// // Описываем структуру стека
-// struct Stack {
-//     int top; // Индекс верхнего элемента стека
-//     int capacity; // Максимальная емкость стека
-//     int *array; // Указатель на динамический массив, хранящий элементы стека
-// };
-
-// // Функция создает новый стек с заданной емкостью
-// struct Stack *createStack(int capacity) {
-//     struct Stack *stack = (struct Stack*) malloc(sizeof(struct Stack)); // Выделяем память под стек
-//     stack->capacity = capacity; // Устанавливаем емкость стека
-//     stack->top = -1; // Устанавливаем индекс верхнего элемента в -1, так как стек пока пустой
-//     stack->array = (int*) malloc(stack->capacity * sizeof(int)); // Выделяем память под динамический массив
-//     return stack; // Возвращаем указатель на созданный стек
-// }
-
-// // Функция проверяет, заполнен ли стек
-// int isFull(struct Stack *stack) {
-//     return stack->top == stack->capacity - 1; // Если индекс верхнего элемента равен максимальной емкости стека - 1, то стек заполнен
-// }
-
-// // Функция проверяет, пуст ли стек
-// int isEmpty(struct Stack *stack) {
-//     return stack->top == -1; // Если индекс верхнего элемента равен -1, то стек пуст
-// }
-
-// // Функция добавляет элемент в стек
-// void push(struct Stack *stack, int item) {
-//     if (isFull(stack)) {
-//         printf("Error: Stack overflow\n");
-//         return;
-//     }
-//     stack->array[++stack->top] = item; // Увеличиваем индекс верхнего элемента и добавляем элемент в массив
-// }
-
-// // Функция удаляет верхний элемент из стека
-// int pop(struct Stack *stack) {
-//     if (isEmpty(stack)) {
-//         printf("Error: Stack is empty\n");
-//         return -1;
-//     }
-//     return stack->array[stack->top--]; // Уменьшаем индекс верхнего элемента и возвращаем его значение
-// }
-
-// // Функция возвращает верхний элемент стека
-// int peek(struct Stack *stack) {
-//     if (isEmpty(stack)) {
-//         printf("Error: Stack is empty\n");
-//         return -1;
-//     }
-//     return stack->array[stack->top]; // Возвращаем значение верхнего элемента стека
-// }
-
-// // Функция удаляет стек
-// void deleteStack(struct Stack *stack) {
-//     if (stack) {
-//         if (stack->array) {
-//             free(stack->array);
-//         }
-//         free(stack);
-//     }
-// }
-
-// int main() {
-//     struct Stack *stack = createStack(100); // Создаем стек с емкостью 100
-//     push(stack, 10); // Добавляем элемент 10 в стек
-//     push(stack, 20); // Добавляем элемент 20 в стек
-//     push(stack, 30); // Добавляем элемент 30 в стек
-//     printf("Top element is %d\n", peek(stack)); // Выводим верхний элемент стека
-//     printf("Popped element is %d\n", pop(stack)); // Удаляем верхний элемент стека и выводим его значение
-//     deleteStack(stack); // Удаляем стек
-//     return 0;
-// }
-
-// Структура для хранения стека
-struct Stack {
-    int top;
-    int capacity;
-    int *array;
-};
-
-// Функция создает стек с заданной емкостью
 struct Stack *createStack(int capacity) {
     struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
     stack->top = -1;
     stack->capacity = capacity;
-    stack->array = (int *)malloc(stack->capacity * sizeof(int));
+    stack->data = (char*)malloc(stack->capacity * sizeof(char));
     return stack;
 }
 
-// Функция проверяет, пуст ли стек
-int isEmpty(struct Stack *stack) {
+int is_empty(struct Stack *stack) {
     return stack->top == -1;
 }
 
-// Функция вычисляет количество элементов в стеке
 int size(struct Stack *stack) {
     return stack->top + 1;
 }
 
-// Функция удаляет стек
-void deleteStack(struct Stack *stack) {
+void delete_stack(struct Stack *stack) {
     if (stack) {
-        if (stack->array) {
-            free(stack->array);
+        if (stack->data) {
+            free(stack->data);
         }
         free(stack);
     }
 }
 
-// Функция увеличивает емкость стека
-void resizeStack(struct Stack *stack) {
+void resize_stack(struct Stack *stack) {
     int newCapacity = stack->capacity * 2;
-    int *newArray = (int *)realloc(stack->array, newCapacity * sizeof(int));
-    if (newArray) {
-        stack->array = newArray;
+    char *newdata = (char*)realloc(stack->data, newCapacity * sizeof(char));
+    if (newdata) {
+        stack->data = newdata;
         stack->capacity = newCapacity;
     }
 }
 
-// Функция добавляет элемент в стек
-void push(struct Stack *stack, int item) {
+void push(struct Stack *stack, char item) {
     if (size(stack) == stack->capacity) {
-        resizeStack(stack);
+        resize_stack(stack);
     }
-    stack->array[++stack->top] = item;
+    stack->data[++stack->top] = item;
 }
 
-// Функция возвращает верхний элемент стека
-int peek(struct Stack *stack) {
-    if (isEmpty(stack)) {
+char peek(struct Stack *stack) {
+    if (is_empty(stack)) {
         return -1;
     }
-    return stack->array[stack->top];
+    return stack->data[stack->top];
 }
 
-// Функция удаляет верхний элемент стека и возвращает его
-int pop(struct Stack *stack) {
-    if (isEmpty(stack)) {
+char pop(struct Stack *stack) {
+    if (is_empty(stack)) {
         return -1;
     }
-    return stack->array[stack->top--];
+    return stack->data[stack->top--];
+}
+
+void is_func(struct Stack *stack, char *string, int *index) {
+    if (string[*index] == 'c') {
+        if (string[*index + 1] == 'o') {
+            if (string[*index + 2] == 's') {
+                if (string[*index + 3] == '(') {
+                    push(stack, 'c');
+                    // *index += 3;
+                }
+            }
+        }
+    }
+    if (string[*index] == 's') {
+        if (string[*index + 1] == 'i') {
+            if (string[*index + 2] == 'n') {
+                if (string[*index + 3] == '(') {
+                    push(stack, 's');
+                    // *index += 3;
+                }
+            }
+        }
+    }
+    if (string[*index] == 't') {
+        if (string[*index + 1] == 'a') {
+            if (string[*index + 2] == 'n') {
+                if (string[*index + 3] == '(') {
+                    push(stack, 't');
+                    // *index += 3;
+                }
+            }
+        }
+    }
+    if (string[*index] == 'a') {
+        if (string[*index + 1] == 'c') {
+            if (string[*index + 2] == 'o') {
+                if (string[*index + 3] == 's') {
+                    if (string[*index + 4] == '(') {
+                        push(stack, 'a');
+                        // *index += 4;
+                    }
+                }
+            }
+        }
+    }
+    if (string[*index] == 'a') {
+        if (string[*index + 1] == 's') {
+            if (string[*index + 2] == 'i') {
+                if (string[*index + 3] == 'n') {
+                    if (string[*index + 4] == '(') {
+                        push(stack, 'a');
+                        // *index += 4;
+                    }
+                }
+            }
+        }
+    }
+    if (string[*index] == 'a') {
+        if (string[*index + 1] == 't') {
+            if (string[*index + 2] == 'a') {
+                if (string[*index + 3] == 'n') {
+                    if (string[*index + 4] == '(') {
+                        push(stack, 'a');
+                        // *index += 4;
+                    }
+                }
+            }
+        }
+    }
+    if (string[*index] == 's') {
+        if (string[*index + 1] == 'q') {
+            if (string[*index + 2] == 'r') {
+                if (string[*index + 3] == 't') {
+                    if (string[*index + 4] == '(') {
+                        push(stack, '~');
+                        // *index += 4;
+                    }
+                }
+            }
+        }
+    }
+    if (string[*index] == 'l') {
+        if (string[*index + 1] == 'n') {
+            if (string[*index + 2] == '(') {
+                push(stack, 'l');
+                // *index += 2;
+            }
+        }
+    }
+    if (string[*index] == 'l') {
+        if (string[*index + 1] == 'o') {
+            if (string[*index + 2] == 'g') {
+                if (string[*index + 3] == '(') {
+                    push(stack, '!');
+                    // *index += 3;
+                }
+            }
+        }
+    }
+    if (string[*index] == 'p') {
+        if (string[*index + 1] == 'o') {
+            if (string[*index + 2] == 'w') {
+                if (string[*index + 3] == '(') {
+                    push(stack, 'p');
+                    // *index += 3;
+                }
+            }
+        }
+    }
+    if (string[*index] == 'm') {
+        if (string[*index + 1] == 'o') {
+            if (string[*index + 2] == 'd') {
+                if (string[*index + 3] == '(') {
+                    push(stack, 'm');
+                    // *index += 3;
+                }
+            }
+        }
+    }
+    if (string[*index] == '(') {
+        push(stack, '(');
+    }
 }
 
 double polish_notation(char *string) {
+    struct Stack *stack = createStack(2);
+    int index = 0;
     double result = 0.0;
+    char output[255] = {'\0'};
+    for (int i = 0; i < strlen(string) + 1; i++) {
+        switch (string[i]) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                output[index] = string[i];
+                index++;
+                break;
+            case 'c':
+            case 's':
+            case 't':
+            case 'a':
+            case 'l':
+            case 'p':
+            case 'm':
+            case '(':
+                is_func(stack, string, &i);
+                break;
+            case ',':
+                while (peek(stack) != '(') {
+                    output[index] = pop(stack);
+                    index++;
+                }
+                break;
+            case '*':
+                while (peek(stack) != '-' || peek(stack) != '+') {
+                    output[index] = pop(stack);
+                    index++;
+                }
+                push(stack, '*');
+                break;
+            case '/':
+                while (peek(stack) != '-' || peek(stack) != '+') {
+                    output[index] = pop(stack);
+                    index++;
+                }
+                push(stack, '/');
+                break;
+            case '+':
+                push(stack, '+');
+                break;
+            case '-':
+                push(stack, '-');
+                break;
+            case ')':
+                while (peek(stack) != '(') {
+                    output[index] = pop(stack);
+                    index++;
+                }
+                pop(stack);
+                while (peek(stack) == 'c' || peek(stack) == 's' || peek(stack) == 't' || peek(stack) == 'a' || peek(stack) == 'p' || peek(stack) == 'm' || peek(stack) == '~' || peek(stack) == '!' || peek(stack) == 'l') {
+                    output[index] = pop(stack);
+                    index++;
+                }
+                break;
+            case '\0':
+                while (!is_empty(stack)) {
+                    output[index] = pop(stack);
+                    index++;
+                }
+                break;
+        }
+    }
+    printf("Result - %s\n", output);
+    delete_stack(stack);
     return result;
 }
 
 int main() {
-    struct Stack *stack = createStack(2);
-    push(stack, 10);
-    push(stack, 20);
-    push(stack, 30);
-    printf("%d popped from stack\n", pop(stack));
-
-    printf("Top element is %d\n", peek(stack));
-
-    deleteStack(stack);
-
+    polish_notation("2+(3-4)+3-7+pow(10, 2)");
     return 0;
 }
--- src/s21_SmartCalc.h
+++ src/s21_SmartCalc.h
@@ -3,8 +3,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
+#define COS 1
+#define SIN 2
+#define TAN 3
+#define ACOS 4
+#define ASIN 5
+#define ATAN 6
+#define SQRT 7
+#define LN 8
+#define LOG 9
+
+struct Stack {
+    int top;
+    int capacity;
+    char *data;
+};
+
 double polish_notation(char *string);
+void is_func(struct Stack *stack, char *string, int *index);
 
 #endif // SRC_S21_SMARTCALC_H_
